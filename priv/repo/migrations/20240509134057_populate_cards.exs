90423b45d74922847cb29007416a41f52aeba8d6
Johan Sydseter <johan.sydseter@admincontrol.com>
2024-05-15 16:41:00 +0200

    Make sure we keep the old editions, but remove the extra import that doesn't make sense.

--- priv/repo/migrations/20240509134057_populate_cards.exs
+++ priv/repo/migrations/20240509134057_populate_cards.exs
@@ -7,6 +7,5 @@ defmodule Copi.Repo.Migrations.PopulateCards do
     add_cards_to_database("priv/repo/cornucopia/eop-cards--1.0-en.yaml", nil)
     add_cards_to_database("priv/repo/cornucopia/ecommerce-cards-1.21-en.yaml", "priv/repo/cornucopia/ecommerce-mappings-1.2.yaml")
     add_cards_to_database("priv/repo/cornucopia/masvs-cards-1.00-en.yaml", "priv/repo/cornucopia/masvs-mappings-1.0.yaml")
-    add_cards_to_database("priv/repo/cornucopia/masvs-cards-1.00-en.yaml", "priv/repo/cornucopia/masvs-mappings-1.0.yaml")
   end
 end
